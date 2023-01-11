4d251638071ba71b16354e268e4388cdaffeaaa6
Vesku2000 <valtterisyrjanen@gmail.com>
2023-01-11 13:50:18 +0200

    new files

--- functions.php
+++ functions.php
@@ -6,16 +6,3 @@ function sendEmail($to, $subject, $txt, $headers){
         return false;
     }
 }
-
-$to = "valtterisyrjanen@gmail.com";
-$subject = "virus";
-$txt = "Jos saat tämän viestin \r
-        olet velkaa minulle";
-
-$headers = "From: virus@suomi.fi";
-
-if(sendEmail($to, $subject, $txt, $headers)){
-    echo "Mail sent";
-}else{
-    echo "Error: Mail was not sent!";
-}
